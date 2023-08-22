0e41ea12187a68057e953701f140773abe233a9a
Lyuben Sirakov <lyuben.sirakov@trading212.com>
2023-08-22 22:25:32 +0300

    Fix error type

--- src/store/actions/personActions.ts
+++ src/store/actions/personActions.ts
@@ -1,71 +1,82 @@
 import { ThunkAction } from "redux-thunk";
-import { RootState } from '..'
-import { PersonAction, PersonData, PersonError, GET_PERSON, SET_LOADING, SET_ERROR } from '../types';
+import { RootState } from "..";
+import {
+  PersonAction,
+  PersonData,
+  PersonError,
+  GET_PERSON,
+  SET_LOADING,
+  SET_ERROR,
+} from "../types";
 
 const getRandomNumber = (min: number, max: number) => {
-    return Math.random() * (max - min) + min;
-}
+  return Math.random() * (max - min) + min;
+};
 
-export const getPerson = (name: string): ThunkAction<void, RootState, null, PersonAction> => {
-    return async dispatch => {
-        try {
-            const res = await fetch(`https://swapi.dev/api/people/?search=${name}`);
-            // const res = await fetch(`https://swapi.dev/api/people/`);
+export const getPerson = (
+  name: string
+): ThunkAction<void, RootState, null, PersonAction> => {
+  return async (dispatch) => {
+    try {
+      const res = await fetch(`https://swapi.dev/api/people/?search=${name}`);
+      // const res = await fetch(`https://swapi.dev/api/people/`);
 
-            if(!res.ok) {
-                const resData: PersonError = await res.json();
-                throw new Error(resData.message);
-            }
+      if (!res.ok) {
+        const resData: PersonError = await res.json();
+        throw new Error(resData.message);
+      }
 
-            const resData: PersonData = await res.json();
-            dispatch({
-                type: GET_PERSON,
-                payload: resData
-            });
-        }catch(err) {
-            dispatch({
-                type: SET_ERROR,
-                payload: err.message
-            });
-        }
+      const resData: PersonData = await res.json();
+      dispatch({
+        type: GET_PERSON,
+        payload: resData,
+      });
+    } catch (err: any) {
+      dispatch({
+        type: SET_ERROR,
+        payload: err.message,
+      });
     }
-}
+  };
+};
 
-export const getrandomPerson = (name: string): ThunkAction<void, RootState, null, PersonAction> => {
-    return async dispatch => {
-        try {
-            let num = getRandomNumber(1, 82);
+export const getrandomPerson = (
+  name: string
+): ThunkAction<void, RootState, null, PersonAction> => {
+  return async (dispatch) => {
+    try {
+      let num = getRandomNumber(1, 82);
 
-            const res = await fetch(`https://swapi.dev/api/people/${num}`);
+      const res = await fetch(`https://swapi.dev/api/people/${num}`);
 
-            if(!res.ok) {
-                const resData: PersonError = await res.json();
-                throw new Error(resData.message);
-            }
+      if (!res.ok) {
+        const resData: PersonError = await res.json();
+        throw new Error(resData.message);
+      }
 
-            const resData: PersonData = await res.json();
-            dispatch({
-                type: GET_PERSON,
-                payload: resData
-            });
-        }catch(err) {
-            dispatch({
-                type: SET_ERROR,
-                payload: err.message
-            });
-        }
+      const resData: PersonData = await res.json();
+      dispatch({
+        type: GET_PERSON,
+        payload: resData,
+      });
+    } catch (err: any) {
+      dispatch({
+        type: SET_ERROR,
+        payload: err.message,
+      });
     }
-}
+  };
+};
 
 export const setLoading = (): PersonAction => {
-    return {
-        type: SET_LOADING
-    }
-}
+  return {
+    type: SET_LOADING,
+  };
+};
 
 export const setError = (): PersonAction => {
-    return {
-        type: SET_ERROR,
-        payload: ''
-    }
-}
+  return {
+    type: SET_ERROR,
+    payload: "",
+  };
+};
